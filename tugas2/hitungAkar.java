6ba3b0211a66c90e9f0983b223c906d72c1c6224
serevinaash <serevinasherly02@gmail.com>
2023-09-13 14:22:51 +0700

    tugasjava2

--- tugas2/hitungAkar.java
+++ tugas2/hitungAkar.java
@@ -1,40 +1,29 @@
 import java.util.Scanner;
 
-public class HitungAkar{
+public class hitungAkar {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        // Menghitung akar-akar persamaan kuadrat
-        System.out.println("Masukkan koefisien A: ");
+        System.out.println("Masukkan nilai A:");
         double A = input.nextDouble();
-        System.out.println("Masukkan koefisien B: ");
+
+        System.out.println("Masukkan nilai B:");
         double B = input.nextDouble();
-        System.out.println("Masukkan koefisien C: ");
+
+        System.out.println("Masukkan nilai C:");
         double C = input.nextDouble();
 
         double D = B * B - 4 * A * C;
 
         if (D < 0) {
-            System.out.println("Akar-akar persamaan adalah akar imajiner.");
+            System.out.println("Akar imajiner.");
         } else if (D == 0) {
             double X1 = -B / (2 * A);
-            System.out.println("Akar-akar persamaan adalah X1 = X2 = " + X1);
+            System.out.println("Akar tunggal: X1 = " + X1);
         } else {
             double X1 = (-B + Math.sqrt(D)) / (2 * A);
             double X2 = (-B - Math.sqrt(D)) / (2 * A);
-            System.out.println("Akar-akar persamaan adalah X1 = " + X1 + " dan X2 = " + X2);
-        }
-
-        // Menentukan kondisi fisik berdasarkan suhu dalam Celsius
-        System.out.println("Masukkan suhu dalam Celsius: ");
-        double suhuCelsius = input.nextDouble();
-
-        if (suhuCelsius < 0) {
-            System.out.println("Kondisi fisik: Padat");
-        } else if (suhuCelsius >= 0 && suhuCelsius <= 100) {
-            System.out.println("Kondisi fisik: Cair");
-        } else {
-            System.out.println("Kondisi fisik: Gas");
+            System.out.println("Akar-akar: X1 = " + X1 + ", X2 = " + X2);
         }
     }
 }
