42bfd8fefdd0a00e1d744fa678bd0c950500bc19
serevinaash <serevinasherly02@gmail.com>
2023-09-13 14:14:00 +0700

    java

---
+++ tugas2/tugas2.1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class hitungAkar{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Menghitung akar-akar persamaan kuadrat
+        System.out.println("Masukkan koefisien A: ");
+        double A = input.nextDouble();
+        System.out.println("Masukkan koefisien B: ");
+        double B = input.nextDouble();
+        System.out.println("Masukkan koefisien C: ");
+        double C = input.nextDouble();
+
+        double D = B * B - 4 * A * C;
+
+        if (D < 0) {
+            System.out.println("Akar-akar persamaan adalah akar imajiner.");
+        } else if (D == 0) {
+            double X1 = -B / (2 * A);
+            System.out.println("Akar-akar persamaan adalah X1 = X2 = " + X1);
+        } else {
+            double X1 = (-B + Math.sqrt(D)) / (2 * A);
+            double X2 = (-B - Math.sqrt(D)) / (2 * A);
+            System.out.println("Akar-akar persamaan adalah X1 = " + X1 + " dan X2 = " + X2);
+        }
+
+        // Menentukan kondisi fisik berdasarkan suhu dalam Celsius
+        System.out.println("Masukkan suhu dalam Celsius: ");
+        double suhuCelsius = input.nextDouble();
+
+        if (suhuCelsius < 0) {
+            System.out.println("Kondisi fisik: Padat");
+        } else if (suhuCelsius >= 0 && suhuCelsius <= 100) {
+            System.out.println("Kondisi fisik: Cair");
+        } else {
+            System.out.println("Kondisi fisik: Gas");
+        }
+    }
+}
--- tugas2/tugas2.java
+++ tugas2/tugas2.java
@@ -1,13 +1,20 @@
-class TahunKabisat {int tahun;}
-class tugas2{public static void main(String[]args){
-    Scanner input = new Scanner(System.in);
-    System.out.print("Masukan Tahun : ");
-    int tahun = input.nextInt();
+import java.util.Scanner;
 
-    if((tahun % 4 == 0 && tahun % 100 != 0)|| (tahun % 400 == 0 )){
-        System.out.println(tahun + "adalah tahun kabisat");
-    }else{
-        System.out.println(tahun + "bukan tahun kabisat");
-    }
+public class tugas2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan Tahun : ");
+
+        try {
+            int tahun = input.nextInt();
+
+            if ((tahun % 4 == 0 && tahun % 100 != 0) || (tahun % 400 == 0)) {
+                System.out.println(tahun + " adalah tahun kabisat");
+            } else {
+                System.out.println(tahun + " bukan tahun kabisat");
+            }
+        } catch (java.util.InputMismatchException e) {
+            System.out.println("Masukan tidak valid. Harap masukkan tahun dalam bentuk angka.");
+        }
     }
 }
