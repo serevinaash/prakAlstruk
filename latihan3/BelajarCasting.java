c6f1dd421aefef87a8a8fe2a2648ddc5a41b341a
serevinaash <serevinasherly02@gmail.com>
2023-09-20 08:50:12 +0700

    belajarcasting

---
+++ latihan3/BelajarCasting.java
@@ -0,0 +1,8 @@
+public class BelajarCasting {
+    public static void main(String[] args) {
+        int nilai = 9; 
+        String duapuluh = "20";
+        int ubah = Integer.parseInt(duapuluh);
+        System.out.println("Tanggal Lahir Saya = " + (nilai + ubah));
+    }
+}
