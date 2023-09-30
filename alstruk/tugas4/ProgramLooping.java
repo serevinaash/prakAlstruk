dd797eb01d4e3073b2f0024592e9e9075e4471d4
RayRama <rayramadita12@gmail.com>
2023-09-30 14:58:47 +0700

    Fix: Change to while loop & fixing the logic

--- alstruk/tugas4/ProgramLooping.java
+++ alstruk/tugas4/ProgramLooping.java
@@ -1,16 +1,33 @@
 public class ProgramLooping {
     public static void main(String[] args) {
-        for (int a = 1; a < 1000; a++) {
-            for (int b = a + 1; b < 1000; b++) {
-                double result = (Math.pow(a, 2) + Math.pow(b, 2) + 1) / (a * b);
+        // for (int a = 1; a < 10; a++) {
+        //     for (int b = a + 1; b < 10; b++) {
+        //         double result = (Math.pow(a, 2) + Math.pow(b, 2) + 1) / (a * b);
 
-                if (result > 1.0 && result < 2.0) {
-                    System.out.println("Hasil yang memenuhi kondisi ditemukan:");
-                    System.out.println("a = " + a + ", b = " + b);
-                    System.out.println("(a^2 + b^2 + 1) / (a * b) = " + result);
-                    System.out.println("------------------------------");
-                }
+        //         System.out.println("Hasil yang memenuhi kondisi ditemukan:");
+        //         if (result > 1.0 && result < 2.0) {
+        //             System.out.println("a = " + a + ", b = " + b);
+        //             System.out.println("(a^2 + b^2 + 1) / (a * b) = " + result);
+        //             System.out.println("------------------------------");
+        //         }
+        //     }
+        // }
+        int a = 1, b = 2;
+        int n = 100; // Ganti ke 1000
+
+        while (a <= n && b <= n){
+            double result = (Math.pow(a, 2) + Math.pow(b, 2) + 1) / (a * b);
+            double finalResult = Math.round(result * 100.0) / 100.0;
+            
+            if (finalResult <= 2.0) {
+                System.out.println("Hasil yang memenuhi kondisi ketika:" + " a = " + a + ", b = " + b + " dengan hasil " + finalResult + "\n");
+                // System.out.println("(a^2 + b^2 + 1) / (a * b) = " + finalResult);
+                // System.out.println("------------------------------");
             }
+        
+            a++;
+            b++;
         }
+       
     }
 }
